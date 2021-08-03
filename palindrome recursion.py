b4ec0a463acaf700660aa2b421d3d431af3dd9bb
Mitra Teja <82991955+amitrateja@users.noreply.github.com>
2021-08-03 10:36:24 +0530

    Delete palindrome recursion.py

--- palindrome recursion.py
+++
@@ -1,8 +0,0 @@
-def rec_palindrome(a):
-    if len(a)<1:
-        print("palindrome")
-    else:
-        if a[0]==a[-1]:
-            return rec_palindrome(a[1:-1])
-        else:
-            print("not palindrome")
